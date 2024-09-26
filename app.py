8b350bdbfee92744d36a23c8ac2fa863b958e517
Julia Porrino <44690146+who0liebo0lie@users.noreply.github.com>
2024-09-26 19:56:42 -0400

    updat app,py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import pandas as pd 
 import streamlit as st
 import plotly.express as px
-vehicles=pd.read_csv('../vehicles_us.csv')
+vehicles=pd.read_csv('./vehicles_us.csv')
 
 # Data clearning
 #check for duplicates
@@ -32,7 +32,7 @@ vehicles_edited=vehicles[vehicles['model_year'] !=0]
 vehicles_edited['model_year'].min()
 #calculate age of each car
 vehicles_edited['age_of_car']=vehicles_edited['year_posted']-vehicles_edited['model_year']
-vehicles_edited.head()
+
 
 # task 1 show your data
 #Filter to find unique manufacturers
@@ -44,7 +44,7 @@ manufacturer_type=vehicles.groupby('manufacturer')['price'].sum().reset_index()
 #representation of if a diesel or gas car sells faster 
 gas_type=vehicles.groupby('fuel').agg({'days_listed':'median'})
 #what is the most common color for an Acura 
-visual=vehicles_year.groupby(['manufacturer','paint_color'])['paint_color'].count()
+visual=vehicles_edited.groupby(['manufacturer','paint_color'])['paint_color'].count()
 visual.head()
 # task 2 draw a plot
 
@@ -56,13 +56,13 @@ st.write('Select the manufacturer from dopdown to filter data')
 select_menu = st.selectbox('Select the manufacturer',manufacturer_choice)
 
 #create a slider to select the year
-min_year, max,year=int(vehicles['model_year'][vehicles['model_year'] != 0].min()), int(vehicles['model_year'].max())
+min_year, max_year=int(vehicles['model_year'][vehicles['model_year'] != 0].min()), int(vehicles['model_year'].max())
 year_range=st.slider('Choose beginning and ending years',value= (min_year, max_year), min_value=min_year, max_value=max_year)
 year_range[0]
 year_range[1]
 
 user_year_range=list(range(year_range[0], year_range[1]+1))
-vehicles_filtered=vehicles[(vehicles['manufacturer_choice'== selected_menu) & (vehicles['model_year'].isin(list(user_year_range)))]]
+vehicles_filtered=vehicles[(vehicles['manufacturer'] == select_menu) & (vehicles.model_year.isin(list(user_year_range)))]
 st.table(vehicles_filtered)
 
 
@@ -70,9 +70,10 @@ st.header('Used Car Price Analysis')
 st.write('Find which factors influence price the most:transmission, fuel, body type or running condition')
 list_for_hist=['transmission', 'fuel', 'type', 'condition']
 select_variable = st.selectbox('Select the factor to evaluate for price distribution', list_for_hist)
+
 #create a figure
-fig1 = px.histogram(vehicles, x='price', color=selecct_variable)
-fig1.update_layout(title="Price effect of {}</b".format(select_variable))
+fig1 = px.histogram(vehicles, x='price', color=select_variable)
+fig1.update_layout(title="<b> Price effect of {}<b>".format(select_variable))
 st.plotly_chart(fig1)
 
 def age_of_car(x):
@@ -82,12 +83,12 @@ def age_of_car(x):
     elif x>=15 and x<20: return '15-20'
     else: return '>20'
 
-vehicles_edited['age_category']=vehicles_edited[age_of_car].apply(age_of_car)
+vehicles_edited['age_category']=vehicles_edited['age_of_car'].apply(age_of_car)
 
 list_for_scatter = ['odometer', 'cylinders', 'condition'] 
 choice_for_scatter = st.selectbox('Price dependency on', list_for_scatter)
 
-fig2=px.scatter(vehichles_edited, x='price', y=choice_for_scatter, color='age_of_car')
-fig2.update_layout(title="Price effect of {}</b".format(choice_for_scatter))
+fig2=px.scatter(vehicles_edited, x='price', y=choice_for_scatter, color='age_of_car')
+fig2.update_layout(title="<b> Price effect of {}<b>".format(choice_for_scatter))
 
 st.plotly_chart(fig2)
